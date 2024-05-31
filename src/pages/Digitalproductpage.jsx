4eb119cf695b687f235ff16f1333b3029be09c01
piyush-github12 <piyushgithubaccouunt@gmail.com>
2024-06-01 02:10:08 +0530

    new

--- src/pages/Digitalproductpage.jsx
+++ src/pages/Digitalproductpage.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react'
+import { AnimatePresence, animate, motion } from "framer-motion";
+import React, { useState } from "react";
 import { FaAngleDown } from "react-icons/fa";
 
-
 const Digitalproductpage = () => {
+  const [oncl, setoncl] = useState(false);
 
-    const [oncl, setoncl] = useState(false)
-
-    const onclickhandler =()=>{
-        
-    }
+  const onhover = () => {
+    animate("#sk",{y:"100%"} , )
+  };
+  const onhoveroff = () => {
+    animate("#sk", { y: "200%" },)
+    
+  };
 
   return (
     <div className="w-full min-h-[100vh] px-[5vw] py-40 text-white">
@@ -38,27 +41,175 @@ const Digitalproductpage = () => {
           </p>
           <div
             id="uiux"
-            className="relative w-full h-[100vh] border-t-[2px] border-[#ffffff2e] -teal-300 mt-20 py-5"
-          > 
-            <div onClick={onclickhandler} className='absolute right-5 top-5 w-[40px] h-[40px] '>
-                <FaAngleDown  className='text-white w-full h-full '/>
-            </div>
-            <h1 className="text-[4vw]">UI UX Design</h1>
-            <div className='w-full flex items-start gap-2 mt-5 '>
+            className="relative w-full h-fit  border-t-[2px] border-[#ffffff2e] -teal-300 mt-20 py-5"
+          >
+            <details open name="ui">
+              <summary>
+                <div
+                  id="summarker"
+                  className="absolute right-5 top-5 w-[40px] h-[40px] "
+                >
+                  <FaAngleDown className="text-white w-full h-full rotate-[-90deg] " />
+                </div>
+                <h1 className="text-[4vw]">UI UX Design</h1>
 
-                {["UX Audit", "UI Design ", "UX Design ", "UX Research"].map((item, index)=>{
-                    return(
-                        <div className='text-xs font-light border-[1px] rounded-full p-2' >
-                            {item}
+                <div className="w-full flex items-start gap-2 mt-10 ">
+                  {["UX Audit", "UI Design ", "UX Design ", "UX Research"].map(
+                    (item, index) => {
+                      return (
+                        <div className="text-xs font-light border-[1px] rounded-full p-2">
+                          {item}
                         </div>
-                    )
-                })}
-            </div>
+                      );
+                    }
+                  )}
+                </div>
+              </summary>
+              <div className="w-full h-[40vw] mt-5">
+                <div
+                  onMouseEnter={onhover}
+                  onMouseLeave={onhoveroff}
+                  className="overflow-hidden relative w-full h-[25%] border-t-[1px] border-[#ffffff2e] flex items-center  gap-10 py-5  "
+                >
+                  
+                    <motion.div
+                      id="sk"
+                      
+                      className="w-full h-full absolute  bg-zinc-600 left-0 top-[-100%] "
+                    ></motion.div>
+               
+                  <div className="w-[30%] h-full font-light text-[30px] z-[99] ">
+                    UI & UX Design
+                  </div>
+                  <div className="w-[50%] h-full z-[99] ">
+                    <p className="font-light">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Nostrum, sit animi fuga temporibus nihil assumenda
+                      corrupti. Magni cupiditate perspiciatis at.
+                    </p>
+                  </div>
+                </div>
+                <div className="w-full h-[25%] border-t-[1px] border-[#ffffff2e] flex items-center  gap-10 py-5  ">
+                  <div className="w-[30%] h-full font-light text-[30px] ">
+                    UI & UX Design
+                  </div>
+                  <div className="w-[50%] h-full  ">
+                    <p className="font-light">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Nostrum, sit animi fuga temporibus nihil assumenda
+                      corrupti. Magni cupiditate perspiciatis at.
+                    </p>
+                  </div>
+                </div>
+                <div className="w-full h-[25%] border-t-[1px] border-[#ffffff2e] flex items-center  gap-10 py-5  ">
+                  <div className="w-[30%] h-full font-light text-[30px] ">
+                    UI & UX Design
+                  </div>
+                  <div className="w-[50%] h-full  ">
+                    <p className="font-light">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Nostrum, sit animi fuga temporibus nihil assumenda
+                      corrupti. Magni cupiditate perspiciatis at.
+                    </p>
+                  </div>
+                </div>
+                <div className="w-full h-[25%] border-t-[1px] border-[#ffffff2e] flex items-center  gap-10 py-5  ">
+                  <div className="w-[30%] h-full font-light text-[30px] ">
+                    UI & UX Design
+                  </div>
+                  <div className="w-[50%] h-full  ">
+                    <p className="font-light">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Nostrum, sit animi fuga temporibus nihil assumenda
+                      corrupti. Magni cupiditate perspiciatis at.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </details>
+          </div>
+          <div
+            id="uiux"
+            className="relative w-full h-fit border-t-[2px] border-[#ffffff2e] -teal-300  py-5"
+          >
+            <details name="ui">
+              <summary>
+                <div
+                  id="summarker"
+                  className="absolute right-5 top-5 w-[40px] h-[40px] "
+                >
+                  <FaAngleDown className="text-white w-full h-full rotate-[-90deg] " />
+                </div>
+                <h1 className="text-[4vw]">Product Design</h1>
+
+                <div className="w-full flex items-start gap-2 mt-10 ">
+                  {["Website Design", "Mobile app ", "Web App ", "SAAS"].map(
+                    (item, index) => {
+                      return (
+                        <div className="text-xs font-light border-[1px] rounded-full p-2">
+                          {item}
+                        </div>
+                      );
+                    }
+                  )}
+                </div>
+              </summary>
+              <div className="w-full h-[40vw] mt-5">
+                <div className="w-full h-[25%] border-t-[1px] border-[#ffffff2e] flex items-center  gap-10 py-5  ">
+                  <div className="w-[30%] h-full font-light text-[30px] ">
+                    UI & UX Design
+                  </div>
+                  <div className="w-[50%] h-full  ">
+                    <p className="font-light">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Nostrum, sit animi fuga temporibus nihil assumenda
+                      corrupti. Magni cupiditate perspiciatis at.
+                    </p>
+                  </div>
+                </div>
+                <div className="w-full h-[25%] border-t-[1px] border-[#ffffff2e] flex items-center  gap-10 py-5  ">
+                  <div className="w-[30%] h-full font-light text-[30px] ">
+                    UI & UX Design
+                  </div>
+                  <div className="w-[50%] h-full  ">
+                    <p className="font-light">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Nostrum, sit animi fuga temporibus nihil assumenda
+                      corrupti. Magni cupiditate perspiciatis at.
+                    </p>
+                  </div>
+                </div>
+                <div className="w-full h-[25%] border-t-[1px] border-[#ffffff2e] flex items-center  gap-10 py-5  ">
+                  <div className="w-[30%] h-full font-light text-[30px] ">
+                    UI & UX Design
+                  </div>
+                  <div className="w-[50%] h-full  ">
+                    <p className="font-light">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Nostrum, sit animi fuga temporibus nihil assumenda
+                      corrupti. Magni cupiditate perspiciatis at.
+                    </p>
+                  </div>
+                </div>
+                <div className="w-full h-[25%] border-t-[1px] border-[#ffffff2e] flex items-center  gap-10 py-5  ">
+                  <div className="w-[30%] h-full font-light text-[30px] ">
+                    UI & UX Design
+                  </div>
+                  <div className="w-[50%] h-full  ">
+                    <p className="font-light">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Nostrum, sit animi fuga temporibus nihil assumenda
+                      corrupti. Magni cupiditate perspiciatis at.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </details>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Digitalproductpage
+export default Digitalproductpage;
